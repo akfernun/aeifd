e9486874ad2aa2f94e933200cf2eeccc36261b5c
Adam Fernung <akfernun@gmail.com>
2014-10-02 11:37:50 -0400

    seed file populates database with scene_type and asset data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Asset.delete_all
+
+apparatus = ['EG29', 'EG53', 'EG54', 'EG61', 'EG62', 'EG63', 'EG64', 'DCEG71', 'L15', 'L19', 'L29', 'L63', 'L64', 'SQ29', 'Tanker 55', 'BG101', 'BG102', 'DCAR71','Safety 06', '1823', '1205', '1103', 'Arson', 'BC12', 'BC13', 'BC14', '1201'];
+
+
+apparatus.each do |name|
+
+	Asset.create(name: name);
+
+end
+
+# populates scene types
+
+SceneType.delete_all;
+
+buildings_string = "1 story
+1 story w/ basement
+2 story
+2 story w/ basement
+3 story
+3 story w/ basement
+Industrial
+Single story
+Multi-story
+High rise (4+ stories)
+Strip mall";
+
+building_types = buildings_string.split("\n");
+
+building_types.each do |type|
+	SceneType.create(name: type);
+end
+
+
+
+
+

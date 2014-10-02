028a7f9a735faf70a79390ef145356fccb46d3cf
Eric Scharnak <eric.scharnak@gmail.com>
2014-10-01 22:41:57 -0400

    Entries Table
    Added entries table on the show page with all the entries for that
    incident.  Still working on getting other entries created other than the
    incident.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,13 +3,27 @@ class Incident < ActiveRecord::Base
   has_many :scene_assignments
   has_many :entries
   
-  after_save :make_entry
+  #after_create :make_entry
+  after_create :create_action
+  after_update :update_action
   
-  accepts_nested_attributes_for :scenes, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+  #accepts_nested_attributes_for :scenes, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :scenes
   accepts_nested_attributes_for :scene_assignments
   
-  def make_entry
-    @entry = Entry.new(incident_id: self.id, name: "Incident - #{self.name} - was created at #{self.created_at}")
-    @entry.save
-  end
+  #def make_entry
+    #@entry = Entry.new(incident_id: self.id, name: "Created Incident - #{self.name}")
+    #@entry.save
+  #end
+
+    def create_action
+      @entry = Entry.new(incident_id: self.id, name: "Created Incident - #{self.name}")
+      @entry.save
+    end
+    
+    def update_action
+      @entry = Entry.new(incident_id: self.id, name: "Updated Incident - #{self.name}")
+      @entry.save
+    end
+
 end
--- db/migrate/20140922223905_add_job_id_to_people.rb
+++
@@ -1,5 +0,0 @@
-class AddJobIdToPeople < ActiveRecord::Migration
-  def change
-    add_column :people, :job_id, :integer
-  end
-end

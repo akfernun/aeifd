9795b177e6425cb42a9b57b6ce604f333f8803cd
Eric Scharnak <eric.scharnak@gmail.com>
2014-10-09 20:16:49 -0400

    Merged Interface Plus Changes
    Moved Nav to top, split up the create incident form.

--- app/models/scene_assignment.rb
+++ app/models/scene_assignment.rb
@@ -6,4 +6,13 @@ class SceneAssignment < ActiveRecord::Base
    belongs_to :incident
    
    accepts_nested_attributes_for :asset
+   
+   after_create :create_action
+   
+   attr_accessor :incident_id
+   
+   def create_action
+    @entry = Entry.new(incident_id: params[:incident_id], name: "This was created by scene assignments")
+    @entry.save
+   end 
 end

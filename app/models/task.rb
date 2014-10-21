9bb7f26250417a6969d07517e97fe392380c45c2
Adam Fernung <akfernun@gmail.com>
2014-10-21 16:12:28 -0400

    edited task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ActiveRecord::Base
 	belongs_to :incident
 
+	before_update :before_update_tasks
+
+	def before_update_tasks
+		self.attributes.each do |k,v|
+			if k != "id" || k!= "incident_id"
+				logger.debug "asd;fjasdlkfjas;ljfa;slkdjf"
+				if v == 1
+					logger.debug "Something is true"
+				end
+			end
+		end
+	end
 end

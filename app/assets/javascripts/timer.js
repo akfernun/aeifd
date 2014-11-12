02b094d9afe8fe00d8ee7b474d66bd235432166f
Adam Fernung <akfernun@gmail.com>
2014-11-11 23:02:03 -0500

    entry is created when benchmarks are checked

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
+    var ending = -15;
 
      $(".example").TimeCircles({use_background:false, text_size: .15, time: {
         Days: {color:"#ffffff" },
         Hours: {color:"#ffffff"},
         Minutes: {color:"#ffffff"},
-        Seconds: {color:"#ffffff"},
-    }});
+        Seconds: {color:"#ffffff"}
+    }}).addListener(
+        function(unit,value,total){
+            // console.log(total);
+            // console.log(ending);
+            if (total== ending){
+                ending += -15;
+                //alert("15 seconds have passed");
+
+                //ajax call here to update when the timer restarted
+
+            }
+        }
+    );
 
 
 
@@ -16,27 +29,14 @@ $(document).ready(function(){
         success: function(data){
         	console.log(data);
 
-        	var all_clear = new Date(data["all_clear"]);
         	var primary_search = new Date(data["primary_search"]);
-        	var utilities_contacted = new Date(data["utilities_contacted"]);
-        	var safety_officer_arrived = new Date(data["safety_officer_arrived"]);
-        	var utilities_disconnected = new Date(data["utilities_disconnected"]);
         	var secondary_search = new Date(data["secondary_search"]);
-        	var fire_under_control = new Date(data["fire_under_control"]);
-
-            $("#all_clear").countdown({until: all_clear, format: "HMS", onExpiry:function(){timerDone("All Clear")}});
 
             $("#primary_search").countdown({until: primary_search, format: "HMS", onExpiry:function(){timerDone("Primary Search")}});
 
-            $("#utilities_contacted").countdown({until: utilities_contacted, format: "HMS", onExpiry:function(){timerDone("Utilities Contacted")}});
-
-            $("#safety_officer_arrived").countdown({until: safety_officer_arrived, format: "HMS", onExpiry:function(){timerDone("Safety Officer Arrived")}});
-
-            $("#utilities_disconnected").countdown({until: utilities_disconnected, format: "HMS", onExpiry:function(){timerDone("Utilities Disconnected")}});
 
             $("#secondary_search").countdown({until: secondary_search, format: "HMS", onExpiry:function(){timerDone("Secondary Search")}});
 
-            $("#fire_under_control").countdown({until: fire_under_control, format: "HMS", onExpiry:function(){timerDone("Fire Under Control")}});
 
         }
     });
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,11 +4,14 @@ class Task < ActiveRecord::Base
 	before_update :before_update_tasks
 
 	def before_update_tasks
+		before_update_record = Task.find(self.id)
+		# logger.debug "this is the before #{before_update_record[:id]}"
 		self.attributes.each do |k,v|
-			if k != "id" || k!= "incident_id"
-				logger.debug "asd;fjasdlkfjas;ljfa;slkdjf"
-				if v == 1
-					logger.debug "Something is true"
+			unless k=="id" or k == "incident_id" or k=="updated_at" or k=="created_at"
+				unless v == before_update_record[k]
+
+					@entry = Entry.new(incident_id: self.incident_id, name: "Benchmark: #{k} was completed.")
+    				@entry.save
 				end
 			end
 		end

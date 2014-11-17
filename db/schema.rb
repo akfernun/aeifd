e4ebda6726597761e1472be776fcefd7a061da8c
Adam Fernung <akfernun@gmail.com>
2014-11-17 16:37:01 -0500

    backend gemfile stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141117192255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "asset_roles", force: true do |t|
     t.string   "name"
     t.datetime "created_at"

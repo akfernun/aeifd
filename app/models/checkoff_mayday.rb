13402d680dc537c54339986a8f4c3d5dcb610988
Adam Fernung <akfernun@gmail.com>
2014-11-24 18:41:55 -0500

    edits to checkoff maydays

--- app/models/checkoff_mayday.rb
+++ app/models/checkoff_mayday.rb
@@ -3,11 +3,12 @@ class CheckoffMayday < ActiveRecord::Base
 	belongs_to :mayday
 	belongs_to :checkoff
 
-	after_update :update_action
+	# after_update :update_action
 
-	def update_action
-		@mayday = Mayday.where(id: self.mayday_id)
-		@entry = Entry.new(:incident_id => @mayday.incident_id, :name => "Mayday Checkoff Completed: #{self.checkoff.name}")
-		@entry.save
-	end
+	# def update_action
+	# 	@mayday = Mayday.where(id: self.mayday_id)
+	# 	@assignment = MaydayAssignment.where(mayday_id: @mayday.id)
+	# 	@entry = Entry.new(:incident_id => @mayday.incident_id, :name => "Mayday Checkoff Completed: #{self.checkoff.name}")
+	# 	@entry.save
+	# end
 end
